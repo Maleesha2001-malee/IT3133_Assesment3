1d2ac84ea8a09d60a6887f2738c9828011a50853
Maleesha Rathnayake <maleesharathnayake840@gmail.com>
2024-11-19 16:56:03 +0000

    base layer

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Student Information Portal</h1>
+      <hr/>
+      <table>
+        <tbody>
+        <tr>
+          <td>
+          </td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
