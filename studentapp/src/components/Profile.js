fb7b0298299cf49d81256cbc2a43384289075e88
Maleesha Rathnayake <maleesharathnayake840@gmail.com>
2024-11-19 17:49:57 +0000

    profile components added

---
+++ studentapp/src/components/Profile.js
@@ -0,0 +1,14 @@
+export default function Profile(props){
+    return(
+        <>
+        <table>
+            <tr>
+                <td rowspan={7}>
+                    <img src={requestAnimationFrame('../assests/css/img/1.jpg')} alt="profile picture"/>
+                </td>
+                <td>ID {props.</td>
+            </tr>
+        </table>
+        </>
+    );
+}
