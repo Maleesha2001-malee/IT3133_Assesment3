cb30226c2720c4044f682640f7d86dde874d4716
Maleesha Rathnayake <maleesharathnayake840@gmail.com>
2024-11-19 17:26:13 +0000

    chande table border

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -4,7 +4,7 @@ export default function StudentTable(props){
         <>
         <div className="outerDiv">
            <div className="leftDiv">
-            <table>
+            <table border="1">
               <thead>
                 <tr>
                     <td>First Name</td>
